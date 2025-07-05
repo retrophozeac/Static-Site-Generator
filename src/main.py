3c6324c355fcf84c0cfc183496cddb05967327fa
Gaurav <f20190899@pilani.bits-pilani.ac.in>
2025-07-05 10:24:02 +0530

    created TextNode

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from textnode import *
+def main():
+    text1 = TextNode("This is some anchor text", TextNodeType.LINK, "https://www.boot.dev")
+    print(text1)
+
+if __name__ == "__main__":
+    main()
