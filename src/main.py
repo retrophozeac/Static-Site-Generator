13a55f87afd9a5988fe9c606598b385b3703c66b
Gaurav <f20190899@pilani.bits-pilani.ac.in>
2025-07-09 12:37:21 +0530

    changes to deploy using Github pages

--- src/main.py
+++ src/main.py
@@ -3,24 +3,30 @@ from textnode import TextType,TextNode
 from block_markdown import markdown_to_html_node
 from static_to_public import static_to_public
 from pathlib import Path
+import sys
 import os
+
 def main():
+    basepath = "/"
+    if len(sys.argv)>1:
+        basepath=sys.argv[1]
+
     static_to_public()
     print("Current working directory:", os.getcwd())
     print("Looking for content at:", os.path.abspath("content/index.md"))
     print("Looking for template at:", os.path.abspath("template.html"))
-    generate_pages_recursive("content/","template.html","public/")
+    generate_pages_recursive("content/","template.html","docs/",basepath)
 
-def generate_pages_recursive(dir_path_content,template_path,dest_dir_path):
+def generate_pages_recursive(dir_path_content,template_path,dest_dir_path,basepath):
     paths = os.listdir(dir_path_content)
     for path in paths:
         curr_path = os.path.join(dir_path_content,path)
         dest_path = os.path.join(dest_dir_path,path)
         if os.path.isfile(curr_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(curr_path,template_path,dest_path)
+            generate_page(curr_path,template_path,dest_path,basepath)
         else:
-            generate_pages_recursive(curr_path,template_path,dest_path)
+            generate_pages_recursive(curr_path,template_path,dest_path,basepath)
 
 def extract_title(markdown):
     lines = markdown.split("\n")
@@ -29,7 +35,7 @@ def extract_title(markdown):
             return line[2:]
     raise Exception("No Header found")
 
-def generate_page(from_path,template_path,dest_path):
+def generate_page(from_path,template_path,dest_path,basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     if os.path.isfile(from_path):
         with open(from_path, 'r') as f:
@@ -42,6 +48,8 @@ def generate_page(from_path,template_path,dest_path):
     title = extract_title(content)
     template =template.replace("{{ Title }}",title)
     template =template.replace("{{ Content }}",html_string)
+    template = template.replace('href="/', 'href="' + basepath)
+    template = template.replace('src="/', 'src="' + basepath)
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
