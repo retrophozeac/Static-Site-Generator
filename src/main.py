d12ceb339fa939e23af244acd1cf11280bcaf8f0
Gaurav <f20190899@pilani.bits-pilani.ac.in>
2025-07-09 12:26:46 +0530

    finished the basic implement of static site generator

--- src/main.py
+++ src/main.py
@@ -2,14 +2,25 @@ from textnode import *
 from textnode import TextType,TextNode
 from block_markdown import markdown_to_html_node
 from static_to_public import static_to_public
+from pathlib import Path
 import os
 def main():
     static_to_public()
     print("Current working directory:", os.getcwd())
     print("Looking for content at:", os.path.abspath("content/index.md"))
     print("Looking for template at:", os.path.abspath("template.html"))
-    generate_page("content/index.md","template.html","public/index.html")
-    
+    generate_pages_recursive("content/","template.html","public/")
+
+def generate_pages_recursive(dir_path_content,template_path,dest_dir_path):
+    paths = os.listdir(dir_path_content)
+    for path in paths:
+        curr_path = os.path.join(dir_path_content,path)
+        dest_path = os.path.join(dest_dir_path,path)
+        if os.path.isfile(curr_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(curr_path,template_path,dest_path)
+        else:
+            generate_pages_recursive(curr_path,template_path,dest_path)
 
 def extract_title(markdown):
     lines = markdown.split("\n")
@@ -31,10 +42,11 @@ def generate_page(from_path,template_path,dest_path):
     title = extract_title(content)
     template =template.replace("{{ Title }}",title)
     template =template.replace("{{ Content }}",html_string)
-    if len(os.path.dirname(dest_path)) != 0:
-        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
-    with open(dest_path,'w') as f:
-        f.write(template)
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
     
 
 
